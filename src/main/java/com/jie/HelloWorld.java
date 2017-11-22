12ed54ea33844bf74d1159cc37f83f8cfab6efc5
xjbian <bianxvjie@foxmail.com>
2017-11-22 23:29:57 +0800

    第一次代码上传

---
+++ src/main/java/com/jie/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jie;
+
+/**
+ * created by xjbian on 2017/11/22
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello.");
+    }
+}
